12f640a65e41848fb514df5363be2cbb08daafb0
ArtemisE1Tara <ahmedsecen2@gmail.com>
2025-03-29 23:45:55 -0400

    fbg

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Public routes that don't require authentication
-  publicRoutes: ["/sign-in", "/sign-up", "/api/webhooks/stripe"]
+  publicRoutes: ["/sign-in", "/sign-up", "/api/webhooks/stripe", "/api/stripe/checkout", "/api/trpc/*", "/api/*"],
 });
 
 export const config = {
